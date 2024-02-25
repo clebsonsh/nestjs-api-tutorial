fb44a702abae120bc562a52aca2c890264cfe99d
Clebson Moura <clebson.sh@gmail.com>
2024-02-24 21:55:34 -0300

    Add auth module, controller, and service

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,58 @@
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import * as argon from 'argon2';
+
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+
+@Injectable()
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
+  async signup(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          password: hash,
+        },
+      });
+
+      delete user.password;
+
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+
+      throw error;
+    }
+  }
+
+  async signin(dto: AuthDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    if (!user) {
+      throw new ForbiddenException('Invalide Credentials');
+    }
+
+    const isPwCorrect = await argon.verify(user.password, dto.password);
+
+    if (!isPwCorrect) {
+      throw new ForbiddenException('Invalide Credentials');
+    }
+
+    delete user.password;
+
+    return user;
+  }
+}
